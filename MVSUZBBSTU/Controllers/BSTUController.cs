16736787ad8e226494088e35868942010bf06f3c
Mirziyod_ms <mirziyodsunatillayev1612@gmail.com>
2022-04-24 03:46:43 +0300

    Добавьте файлы проекта.

---
+++ MVSUZBBSTU/Controllers/BSTUController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVSUZBBSTU.Controllers
+{
+    public class BSTUController : Controller
+    {
+        // GET: BSTU
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Minsk()
+        {
+            return View();
+        }
+        public ActionResult Mogiliov()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVSUZBBSTU/Controllers/UZBController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVSUZBBSTU.Controllers
+{
+    public class UZBController : Controller
+    {
+        // GET: UZB
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        public ActionResult Tash()
+        {
+            return View();
+        }
+        public ActionResult Sam()
+        {
+            return View();
+        }
+        public ActionResult Buxa()
+        {
+            return View();
+        }
+    }
+}
